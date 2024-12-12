b3986ca6efb20b14792d480e85b0276a03013ba3
arjundevasi1406 <ambcomputersofficial@gmail.com>
2024-12-12 10:18:16 +0530

    Update app.py

--- app.py
+++ app.py
@@ -78,4 +78,5 @@ def run_model():
     return jsonify({"result": result})
 
 if __name__ == '__main__':
-    app.run
+    port=int(os.environ.get('PORT',5000))
+    app.run(host='0.0.0.0',port=port)
